c09c65398c54ebc9a02a22ef1c97c3f46a434809
Rajesh1921 <rajeshmudiraj1436@gmail.com>
2026-03-03 18:01:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public'))); // Or wherever your html is
+app.use(express.static(__dirname));
 
 const config = process.env.DB_CONNECTION_STRING;
 
